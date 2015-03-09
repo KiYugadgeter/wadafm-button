83b58a2535151d2145c1ec3160557576007ecf20
KiYugadgeter <zyouhousikaku@gmail.com>
2015-03-10 03:55:33 +0900

    delete audio2

--- wada.js
+++ wada.js
@@ -2,18 +2,13 @@ var wadafm = document.getElementById("wadafm");
 var counter = 0;
 var audio = document.getElementById("wadafm_audio");
 var natami = document.getElementById("natami");
-var m = true;
 wadafm.onclick = function (e) {
-    audio.pause();
     audio.currentTime = 0;
     audio.play();
     counter++;
     if (counter === 100) {
-        natami.pause();
         natami.currentTime = 0;
         counter = 0;
         natami.play();
-
     }
-
-}
+};
