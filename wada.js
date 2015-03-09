b12f658cc82495843076762ea88f74b3d3228521
KiYugadgeter <zyouhousikaku@gmail.com>
2015-03-09 21:09:43 +0900

    secret update

--- wada.js
+++ wada.js
@@ -1,6 +1,8 @@
 var wadafm = document.getElementById("wadafm");
+var counter = 0;
 var audio = document.getElementById("wadafm_audio");
 var audio2 = document.getElementById("wadafm_audio2");
+var natami = document.getElementById("natami");
 var m = true;
 wadafm.onclick = function (e) {
     if (m) {
@@ -14,6 +16,15 @@ wadafm.onclick = function (e) {
         audio.pause();
         audio.currentTime = 0;
         m = true;
+    }
+
+    counter++;
+    if (counter === 100) {
+        natami.pause();
+        natami.currentTime = 0;
+        counter = 0;
+        natami.play();
+
     }
 
 }
