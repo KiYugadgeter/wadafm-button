99625c5f51d6bc7636dfd4b0c8e428bdb8498a06
KiYugadgeter <zyouhousikaku@gmail.com>
2015-03-09 20:18:12 +0900

    support continuous playback

--- wada.js
+++ wada.js
@@ -1,6 +1,19 @@
 var wadafm = document.getElementById("wadafm");
 var audio = document.getElementById("wadafm_audio");
+var audio2 = document.getElementById("wadafm_audio2");
+var m = true;
 wadafm.onclick = function (e) {
-    console.log("clicked");
-    audio.play();
+    if (m) {
+        audio.play();
+        audio2.pause();
+        audio2.currentTime = 0;
+        m = false;
+    }
+    else {
+        audio2.play();
+        audio.pause();
+        audio.currentTime = 0;
+        m = true;
+    }
+
 }
